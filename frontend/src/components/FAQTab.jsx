d8adcffafe11af8aa5f65680ea4ad57c938887cd
kavirasanE <einsteinkavi98@gmail.com>
2024-03-22 18:00:30 +0530

    changes

--- frontend/src/components/FAQTab.jsx
+++ frontend/src/components/FAQTab.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FAQTab = () => {
+    const [open,setOpen] =useState(false)
     return (
         <div className='m-4 mx-20' >
             <div className='bg-white p-4 '>
-                <button className='font-bold pb-5'>What sets us apart from other Desert Safaris operators in Abu Dhabi?</button>
-                <div className='border'></div>
-                <p className='py-5 px-4 gap-2 flex flex-col'>
+                <button className='font-bold' onClick={() => setOpen(!open)}>What sets us apart from other Desert Safaris operators in Abu Dhabi?</button>
+                {open &&
+                <>
+                    <div className='border'></div>
+                    <p className='py-5 px-4 gap-2 flex flex-col'>
                     <li>Thrilling 45-minute Dune Bashing with skilled drivers</li>
+                    
                     <li>Comfortable 4x4 Land Cruiser pickup/drop-off for your Abu Dhabi Desert Safaris</li>
                     <li>24/7 seamless and dedicated customer support</li>
                     <li>Hassle-free Desert Safaris Abu Dhabi booking and cancellation</li>
@@ -15,7 +19,10 @@ const FAQTab = () => {
                     <li>Starlit desert barbecue for a memorable culinary adventure</li>
                     <li>Luxurious traditional camp, a perfect retreat after desert activities</li>
                     <li>Luxurious overnight stay, offering comfort and personalized service in the desert</li>
-                </p>
+                </p> 
+                </>
+                }
+                
             </div>
         </div>
     )
--- frontend/src/pages/Faq.jsx
+++ frontend/src/pages/Faq.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import FAQTab from '../components/FAQTab'
 
 const Faq = () => {
-  return (
-    <div className='bg-slate-600'>
-        szdjvhsdlvnsdlvn
-        <FAQTab/>
-    </div>
-  )
+    const handleToggle = useCallback((e) => {
+            
+    },[]);
+    return (
+        <div className='bg-slate-600'>
+            szdjvhsdlvnsdlvn
+            {[...Array(20)].map((_, index) => (
+                <FAQTab key={index}  handleToggle={handleToggle} />
+            ))}
+        </div>
+    )
 }
 
 export default Faq
