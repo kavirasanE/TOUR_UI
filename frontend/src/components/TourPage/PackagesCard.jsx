d30c86127f64670ee7ec45a20c8f15b4f11747dc
kavirasanE <einsteinkavi98@gmail.com>
2024-03-22 21:23:28 +0530

    changes

--- frontend/src/components/TourPage/PackagesCard.jsx
+++ frontend/src/components/TourPage/PackagesCard.jsx
@@ -3,8 +3,9 @@ import about from "../../assets/about.jpg"
 const PackagesCard = () => {
     return (
         <div className='mt-5'>
-            <div className='w-56'>
-                <img src={about}  className='w-56 h-40 rounded-t-xl'/>
+            <div className='w-56 relative'>
+                <img src={about}  className='w-56 h-40 rounded-t-xl '/>
+                <p className='absolute left-2 top-2  text-xs px-2 py-0.5 rounded-sm bg-gray-100'>Most popular </p>
                 <p className='flex justify-between m-2'>Instant Confirmation <span className='text-yellow-500'>4.9</span></p>
                 <h1 className='font-bold mt-2 text-nowrap truncate'>Most Popular Abhu Dhabi Safari </h1>
                 <p className='line-clamp-3 text-sm my-4'>There is seating for 6 guests + 1 driver. The seats are as follows: 1 next to the driver, 3 in the middle row and 2 in the back row.</p>
